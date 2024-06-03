2be2f96e1984968d1a511aa51edbdc3dec5d09c1
Cesar <20233tn076@utez.edu.mx>
2024-06-02 20:58:59 -0600

    Arreglado el revoltijo que hizo el andrés con los colores, también las olas están más bonitas

--- frontend/js/themeSwitch.js
+++ frontend/js/themeSwitch.js
@@ -3,13 +3,31 @@ const themeIcon = document.getElementById('themeIcon');
 const body = document.body;
 
 themeSwitch.addEventListener('click', () => {
-    body.classList.toggle('light-mode');
+    // const body.classList.toggle('light-mode');
+
+    // if (body.classList.contains('light-mode')) {
+    //     themeIcon.src = 'resources/img/sunIcon.png';
+    //     themeIcon.style.filter = 'invert(0)';
+    //     themeIcon.style.padding = '0 0 0 0';
+    // } else {
+    //     themeIcon.src = 'resources/img/moonIcon.png';
+    //     themeIcon.style.filter = 'invert(1)';
+    //     themeIcon.style.padding = '0 0 0 2px';
+    // }
+
+    const body = document.body;
 
     if (body.classList.contains('light-mode')) {
-        themeIcon.src = 'https://static.thenounproject.com/png/765894-200.png';
-        themeIcon.style.filter = 'invert(0)';
-    } else {
-        themeIcon.src = 'https://static.thenounproject.com/png/4629509-200.png';
+        body.classList.remove('light-mode');
+        body.classList.add('dark-mode');
+        themeIcon.src = 'resources/img/sunIcon.png';
         themeIcon.style.filter = 'invert(1)';
+        themeIcon.style.padding = '0 0 0 0';
+    } else {
+        body.classList.remove('dark-mode');
+        body.classList.add('light-mode');
+        themeIcon.src = 'resources/img/moonIcon.png';
+        themeIcon.style.filter = 'invert(0)';
+        themeIcon.style.padding = '0 0 0 2px';
     }
 });
