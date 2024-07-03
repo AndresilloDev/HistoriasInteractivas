19b66ffcfe66bfc517135980584644da28a2258e
Erick Humberto Teja Carvajal <20233tn060@utez.edu.mx>
2024-07-03 00:19:52 -0600

    Erick commit. Se agregaron un servlet para hacer pruebas y se subio la conexión para mandar correos a traves de gmail, faltan cosas por revisar en UsuarioDao.

--- src/main/java/mx/edu/utez/historiasinteractivas/dao/UsuarioDao.java
+++ src/main/java/mx/edu/utez/historiasinteractivas/dao/UsuarioDao.java
@@ -9,21 +9,22 @@ import java.sql.*;
 public class UsuarioDao {
 
     // Crear usuario en la base de datos
-    public boolean insert(Users u) {
+    public boolean insert(Users usuario) {
         boolean flag = false;
-        String query = "insert into Users(email, name, first_last_name, last_last_name, password, user) values (?, ?, ?, ?, sha2(?, 64), ?);";  // Cambio resaltado
+        String query = "call insertUser(?,?,?,?,?,?,?)";
         try {
             Connection con = DatabaseConnectionManager.getConnection();
-            PreparedStatement ps = con.prepareStatement(query);
-            ps.setString(1, u.getEmail());
-            ps.setString(2, u.getName());
-            ps.setString(3, u.getFirst_last_name());
-            ps.setString(4, u.getLast_last_name());
-            ps.setString(5, u.getPassword());
-            ps.setString(6, u.getUser());
-            if (ps.executeUpdate() > 0) {
-                flag = true;
-            }
+            CallableStatement stmt = con.prepareCall(query);
+            stmt.setString(1,usuario.getEmail());
+            stmt.setString(2,usuario.getUser());
+            stmt.setString(3,usuario.getToken());
+            stmt.setString(4,usuario.getName());
+            stmt.setString(5,usuario.getFirst_last_name());
+            stmt.setString(6,usuario.getLast_last_name());
+            stmt.setString(7,usuario.getPassword());
+            stmt.execute();
+            final ResultSet rs = stmt.getResultSet();
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -31,7 +32,7 @@ public class UsuarioDao {
     }
 
     // Seleccionar usuario de la base de datos
-    public Users getOne(String email, String password){
+    public Users getUserFromEmailANDPassword(String email, String password){
         Users usuario = new Users();
         String query = "select * from Users where email = ? and password = sha2(?,64);";
         try {
@@ -42,12 +43,64 @@ public class UsuarioDao {
             ResultSet rs = ps.executeQuery();
             if(rs.next()){
                 usuario.setEmail(rs.getString("email"));
+                usuario.setUser_type(rs.getInt("user_type"));
+                usuario.setToken(rs.getString("token"));
+                usuario.setName(rs.getString("name"));
+                usuario.setFirst_last_name(rs.getString("first_last_name"));
+                usuario.setLast_last_name(rs.getString("last_last_name"));
                 usuario.setPassword(rs.getString("password"));
+                usuario.setUser(rs.getString("user"));
+                usuario.setStatus(rs.getBoolean("status"));
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
         return usuario;
+    }
+    //Obtener usuario a traves de su correo y token
+    public Users getUserFromEmailANDToken(String email, String token){
+        Users usuario = new Users();
+        String query = "select * from Users where email = ? and token = ?;";
+        try {
+            Connection con = DatabaseConnectionManager.getConnection();
+            PreparedStatement ps = con.prepareStatement(query);
+            ps.setString(1,email);
+            ps.setString(2,token);
+            ResultSet rs = ps.executeQuery();
+            if(rs.next()){
+                usuario.setEmail(rs.getString("email"));
+                //usuario.setUser_type(rs.getInt("user_type"));
+                usuario.setToken(rs.getString("token"));
+                //usuario.setName(rs.getString("name"));
+                //usuario.setFirst_last_name(rs.getString("first_last_name"));
+                //usuario.setLast_last_name(rs.getString("last_last_name"));
+                usuario.setPassword(rs.getString("password"));
+                //usuario.setUser(rs.getString("user"));
+                //usuario.setStatus(rs.getBoolean("status"));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return usuario;
+    }
+
+    public boolean updateUserPassword(String email, String password){
+        boolean flag = false;
+        String query = "update users set password = ? where email = ?";
+        try {
+            Connection con = DatabaseConnectionManager.getConnection();
+            PreparedStatement ps = con.prepareStatement(query);
+            ps.setString(1,password);
+            ps.setString(2,email);
+
+            if (ps.executeUpdate() > 0) {
+                flag = true;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return flag;
     }
 
     public Users existsUser(String email, String password) {
---
+++ src/main/java/mx/edu/utez/historiasinteractivas/utils/GmailSender.java
@@ -0,0 +1,94 @@
+package mx.edu.utez.historiasinteractivas.utils;
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.googleapis.json.GoogleJsonError;
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.gmail.Gmail;
+import com.google.api.services.gmail.model.Message;
+import org.apache.commons.codec.binary.Base64;
+import jakarta.mail.Session;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Paths;
+import java.util.Properties;
+import java.util.Set;
+import static com.google.api.services.gmail.GmailScopes.GMAIL_SEND;
+import static jakarta.mail.Message.RecipientType.TO;
+
+public class GmailSender {
+    private static final String TOKENS_DIRECTORY_PATH = "src/main/resources/tokens";
+    private static final String FROM = "lagunes.derick@gmail.com";
+    private final Gmail service;
+    public GmailSender() throws Exception {
+        NetHttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+        GsonFactory jsonFactory = GsonFactory.getDefaultInstance();
+        service = new Gmail.Builder(httpTransport, jsonFactory, getCredentials(httpTransport, jsonFactory))
+                .setApplicationName("AplicacionesWeb")
+                .build();
+    }
+    private static Credential getCredentials(final NetHttpTransport httpTransport, GsonFactory jsonFactory)
+            throws IOException {
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(jsonFactory,
+                new InputStreamReader(GmailSender.class.getResourceAsStream("/credentials.json")));
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                httpTransport, jsonFactory, clientSecrets, Set.of(GMAIL_SEND))
+                .setDataStoreFactory(new FileDataStoreFactory(Paths.get(TOKENS_DIRECTORY_PATH ).toFile()))
+                .setAccessType("offline")
+                .build();
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+    }
+    public void sendMail(String para, String asunto, String mensaje) throws Exception {
+        Properties props = new Properties();
+        Session session = Session.getDefaultInstance(props, null);
+        MimeMessage email = new MimeMessage(session);
+        email.setFrom(new InternetAddress(FROM));
+        email.addRecipient(TO, new InternetAddress(para));
+        email.setSubject(asunto);
+        email.setContent(mensaje,"text/html; charset=utf-8");
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        email.writeTo(buffer);
+        byte[] rawMessageBytes = buffer.toByteArray();
+        String encodedEmail = Base64.encodeBase64URLSafeString(rawMessageBytes);
+        Message msg = new Message();
+        msg.setRaw(encodedEmail);
+        try {
+            service.users().messages().send("me", msg).execute();
+        } catch (GoogleJsonResponseException e) {
+            GoogleJsonError error = e.getDetails();
+            if (error.getCode() == 403) {
+                System.err.println("No se pudo enviar el mensaje: " + e.getDetails());
+            } else {
+                throw e;
+            }
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        new GmailSender().sendMail("dericklagunes@utez.edu.mx","A new message",
+                "<h1>" +
+                        "Contreras Cortez Cesar Emilio" +
+                        "<br>" +
+                        "García Quiroz Gaspar Andres" +
+                        "<br>" +
+                        "Jimenez Ureña Angel Sebastian" +
+                        "<br>" +
+                        "Garcia Segura Fernanda Gabriela" +
+                        "<br>" +
+                        "Gutierrez Garcia Christopher" +
+                        "<br>" +
+                        "Teja Carvajal Erick Humberto" +
+                        "</h1>"
+        );
+    }
+
+}
