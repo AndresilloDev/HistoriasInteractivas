ac4ee63176611a9fe52c7738857198e306d80d38
Erick Humberto Teja Carvajal <20233tn060@utez.edu.mx>
2024-08-02 22:39:57 -0600

    Servlet de filter terminado

--- src/main/java/mx/edu/utez/historiasinteractivas/controller/FilterEditUserServlet.java
+++ src/main/java/mx/edu/utez/historiasinteractivas/controller/FilterEditUserServlet.java
@@ -10,19 +10,27 @@ import java.io.IOException;
 
 @WebFilter(urlPatterns = {
         "/editUser.jsp",
+        "/adminUsers.jsp",
+        "/index.jsp",
+        "/editUser.jsp",
 })
 public class FilterEditUserServlet implements Filter {
+    @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
         HttpServletResponse httpResponse = (HttpServletResponse) response;
 
+        // Control de caché
+        httpResponse.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1.
+        httpResponse.setHeader("Pragma", "no-cache"); // HTTP 1.0.
+        httpResponse.setDateHeader("Expires", 0); // Proxies.
         HttpSession session = httpRequest.getSession(false);
 
-        if(session == null) {
-            httpResponse.sendRedirect("index.jsp");
+        if(session != null) {
+            chain.doFilter(request,response);
         }
         else {
-            chain.doFilter(request,response);
+            httpResponse.sendRedirect("login.jsp");
         }
     }
 }
