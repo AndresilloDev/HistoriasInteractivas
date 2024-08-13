1714d15610203c67858f969b49708d3438afc2cc
Erick Humberto Teja Carvajal <20233tn060@utez.edu.mx>
2024-08-13 10:29:43 -0600

    Corrección a createStory

--- src/main/java/mx/edu/utez/historiasinteractivas/controller/CreateStoryServlet.java
+++ src/main/java/mx/edu/utez/historiasinteractivas/controller/CreateStoryServlet.java
@@ -44,7 +44,7 @@ public class CreateStoryServlet extends HttpServlet {
 
         if (dao.insertStory(story)) {
             request.setAttribute("message", "¡Historia creada correctamente!");
-            response.sendRedirect("createStory?id_story="+id_story);
+            response.sendRedirect("createStory.jsp?id_story="+id_story);
         } else {
             request.setAttribute("errorMessage", "¡Error al crear la historia!");
             response.sendRedirect("index.jsp");
